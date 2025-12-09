6d1a9f347ca906bc15d70ba85bfea449d981d7b4
Orangeboy115 <puneetpowar10@gmail.com>
2025-12-09 14:29:40 -0500

    Adding Knowledgebase Markdown editor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { TicketViewer } from './page/ticket-viewer/ticket-viewer';
+import { Knowledgebase } from './page/knowledgebase/knowledgebase';
 
-export const routes: Routes = [{ path: '', component: TicketViewer }];
+export const routes: Routes = [
+  { path: '', component: TicketViewer },
+  { path: 'kb', component: Knowledgebase },
+];
---
+++ src/app/page/knowledgebase/knowledgebase.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MarkdownComponent } from 'ngx-markdown';
+
+@Component({
+  selector: 'app-knowledgebase',
+  imports: [CommonModule, FormsModule, MarkdownComponent],
+  templateUrl: './knowledgebase.html',
+  styleUrl: './knowledgebase.css',
+})
+export class Knowledgebase {
+  content: string = localStorage.getItem('kb_draft') || '';
+  preview = false;
+
+  toggleMode() {
+    this.preview = !this.preview;
+  }
+
+  save() {
+    localStorage.setItem('kb_article', this.content);
+    alert('Article saved locally!');
+  }
+
+  autoSave() {
+    localStorage.setItem('kb_draft', this.content);
+  }
+}
